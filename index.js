6d048164d7b5463ff7a93e059cf2b3fc8a3c3130
holdMyGlasses <dusan.rula@gmail.com>
2020-08-13 16:59:45 -0400

    works

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const morgan=require("morgan");
 const app=express();
-const morg =morgan();
-const { db } = require('./models');
+
+const { db, Page, User } = require('./models');
 const layout = require ("./views/layout.js")
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
@@ -13,13 +13,21 @@ app.use(express.urlencoded({ extended: false }));
 app.get ('/',(req, res)=> {
    //res.send ("Hello world");
     res.send (layout(''));
-} )
+})
 
+const synchro = async() => {
 
+  await Page.sync();
+  await User.sync();
+console.log("user and page NOT HEREE", user, page)
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+}
+
+synchro();
 
 db.authenticate()
   .then(() => {
